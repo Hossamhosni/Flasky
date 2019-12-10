2615990f77f71f1db75fad2eb35043a54d25136d
Hossam <hossam.hosni.95@gmail.com>
2019-12-10 22:44:54 +0200

    Add Forms

--- hello.py
+++ hello.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hard to guess string"
 manager = Manager(app)
 boostrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name")
+        session["name"] = form.name.data
+        form.name.data = ""
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get("name"))
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
